c656434168bda5002a5e9f6f81369fbd69344ba2
Thinh Nguyen <Thinh Nguyen>
2016-09-15 01:08:39 +0700

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express');
 //Create our app
 var app = express();
-//xec dinh thu muc de dat server la public
+const PORT = process.env.PORT || 3000;
+//xac dinh thu muc de dat server la public
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 app.use(express.static('public'));
 //start sever takes 2 argument:port and function when it done
-app.listen(3000, function () {
-    console.log('Express sever is up on port 3000');
+app.listen(PORT, function () {
+    console.log('Express sever is up on port '+ PORT);
 });
